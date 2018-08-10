0f18b0d2ef189b4f2468772710c3d3a0cdb5eafc
sandytan10 <stan10@stuy.edu>
2018-08-10 17:28:27 +0000

    automatically backed up by learn

--- spirit_week.rb
+++ spirit_week.rb
@@ -1,4 +1,4 @@
-def()
+def(spirit)
 
 spirit_week = {"Monday" => "Pajama", "Tuesday" => "School pride", "Wednesday" => "pink", 
 "Thursday" => "Disney", "Friday" => "Fancy"}
@@ -7,3 +7,4 @@ spirit_week.each do |days, type|
   puts "The spirit day for #{days} is #{type}"
 
 end
+spirit
